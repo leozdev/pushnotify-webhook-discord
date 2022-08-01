20f15fb4e229c868ebe8aad9f437957a9e9d1020
LeoSupply <110389632+LeoSupply@users.noreply.github.com>
2022-08-01 11:12:14 -0300

    Add files via upload

---
+++ Ticket Bot/events/ready.js
@@ -0,0 +1,88 @@
+module.exports = {
+  name: 'ready',
+  async execute(client) {
+    console.log('Ticket Online!')
+    console.log('Push Notify');
+    const oniChan = client.channels.cache.get(client.config.ticketChannel)
+
+    function sendTicketMSG() {
+      const embed = new client.discord.MessageEmbed()
+        .setColor('ff0000')
+        .setAuthor('Push Ticket', client.user.avatarURL())
+        .setTitle('Sistema de Ticket Automático')
+        .setDescription('Para obter **SUPORTE** abra um ticket clicando o botão a baixo.')
+        .setFooter('Lembre-se não abra um ticket sem necessidade')
+      const row = new client.discord.MessageActionRow()
+        .addComponents(
+          new client.discord.MessageButton()
+          .setCustomId('open-ticket')
+          .setLabel('Abrir Ticket')
+          .setEmoji('🎫')
+          .setStyle('PRIMARY'),
+        );
+
+      oniChan.send({
+        embeds: [embed],
+        components: [row]
+      })
+    }
+
+    const toDelete = 10000;
+
+    async function fetchMore(channel, limit) {
+      if (!channel) {
+        throw new Error(`Canal criado ${typeof channel}.`);
+      }
+      if (limit <= 100) {
+        return channel.messages.fetch({
+          limit
+        });
+      }
+
+      let collection = [];
+      let lastId = null;
+      let options = {};
+      let remaining = limit;
+
+      while (remaining > 0) {
+        options.limit = remaining > 100 ? 100 : remaining;
+        remaining = remaining > 100 ? remaining - 100 : 0;
+
+        if (lastId) {
+          options.before = lastId;
+        }
+
+        let messages = await channel.messages.fetch(options);
+
+        if (!messages.last()) {
+          break;
+        }
+
+        collection = collection.concat(messages);
+        lastId = messages.last().id;
+      }
+      collection.remaining = remaining;
+
+      return collection;
+    }
+
+    const list = await fetchMore(oniChan, toDelete);
+
+    let i = 1;
+
+    list.forEach(underList => {
+      underList.forEach(msg => {
+        i++;
+        if (i < toDelete) {
+          setTimeout(function () {
+            msg.delete()
+          }, 1000 * i)
+        }
+      })
+    })
+
+    setTimeout(() => {
+      sendTicketMSG()
+    }, i);
+  },
+};
